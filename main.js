118cec738bdf370338d42a5a61b5458b2d19656a
Katherine Donnally <kbd3ax@virginia.edu>
2018-07-12 16:24:22 -0400

    Complete 'Hello world' step

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-console.log("test");
+/* FreeCodeCamp React Tutorial */
 
-var container = document.querySelector('#js-container');
+/* ---------------- Step 1: Hello World ---------------- */
 
-container.innerHTML = `
+// vanilla
+var jsContainer = document.querySelector('#js-container');
+
+jsContainer.innerHTML = `
 	<div class="js__demo">
 		Hello, world!
 	</div>
+`;
+
+// React
+var reactContainer = document.querySelector("#react-container");
+
+// create item on page
+ReactDOM.render(
+	// tell me about ur item
+	React.createElement(
+		'div',
+		{ className: "react__demo" },
+		'Hello, React world!'
+	),
+	// and where to put ur item
+	reactContainer
+);
+
+/* ----------------- Step 2: User Input ---------------- */
+
+// vanilla
+jsContainer.innerHTML = `
+<div class="js__demo">
+	Hello! Enter text plz.
+	<input type="text" placeholder="Vanilla JS" aria-label="User input field for Vanilla JavaScript example" />
+</div>
 `;
