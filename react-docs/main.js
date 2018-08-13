c78a3447dcfe79ad5bef374f46590218a26451e3
Katherine Donnally <kbd3ax@virginia.edu>
2018-08-13 17:14:44 -0400

    Compile JSX to JS
    - It works now!

--- react-docs/main.js
+++ react-docs/main.js
@@ -1,30 +1,28 @@
-(function() {
+(function () {
 
-// NB: any subsequent code snippets overwrite earlier ones
+	// NB: any subsequent code snippets overwrite earlier ones
 
-/* ------------- 1. INTRO TO JSX ------------- */
+	/* ------------- 1. INTRO TO JSX ------------- */
 
-// function
-const formatName = user => { 
-	user.firstName + ' ' + user.lastName
-};
+	// function
+	const formatName = user => {
+		return user.firstName + ' ' + user.lastName;
+	};
 
-// input object
-const user = {
-	firstName: 'Seb',
-	lastName: 'Gorka'
-};
+	// input object
+	const user = {
+		firstName: 'Seb',
+		lastName: 'Gorka'
+	};
 
-// JSX w/ embedded expression
-const greeting = (
-	<h1>
-	Hello, {formatName(user)}!
-	</h1>
-);
+	// JSX w/ embedded expression
+	const greeting = React.createElement(
+		'h1',
+		{ className: 'react__demo' },
+		'Hello, ',
+		formatName(user),
+		'!'
+	);
 
-ReactDOM.render(
-	greeting,
-	document.getElementById('root')
-);
-
+	ReactDOM.render(greeting, document.getElementById('root'));
 })();
