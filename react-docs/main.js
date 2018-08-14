a23ffa9b97e0c3e49eb1f9b7632c8ea0334e4710
Katherine Donnally <kbd3ax@virginia.edu>
2018-08-14 16:27:58 -0400

    Add code snippets on render/update elems + component intro

--- react-docs/main.js
+++ react-docs/main.js
@@ -1,7 +1,6 @@
-(function () {
+// NB: any subsequent code snippets overwrite earlier ones
 
-	// NB: any subsequent code snippets overwrite earlier ones
-
+if (document.querySelector('.section-one')) {
 	/* ------------- 1. INTRO TO JSX ------------- */
 
 	// function
@@ -51,4 +50,63 @@
 	}
 
 	ReactDOM.render(getGreeting(user), document.getElementById('root'));
-})();
+
+	/* ------------- 3. UPDATING RENDERED ELEMS ------------- */
+
+	// create div displaying time
+	function tick() {
+		const elem = React.createElement(
+			'div',
+			null,
+			React.createElement(
+				'h1',
+				null,
+				'Hi!!!'
+			),
+			React.createElement(
+				'h2',
+				null,
+				'The time rn is ',
+				new Date().toLocaleTimeString(),
+				'. :)'
+			)
+		);
+		ReactDOM.render(elem, document.getElementById('root'));
+	}
+	// update 1x/second
+	setInterval(tick, 1000);
+
+	/* TAKEAWAY:
+  * only the date displayed changes in browser DOM,
+  * even tho we're "creating" a whole new UI */
+
+	/* ------------- 4. INTRO TO COMPONENTS ------------- */
+
+	// functional component
+	function Welcome(props) {
+		return React.createElement(
+			'h1',
+			null,
+			'Hi ',
+			props.name,
+			'!'
+		);
+	}
+
+	// compose components
+	function App() {
+		return React.createElement(
+			'div',
+			null,
+			React.createElement(Welcome, { name: 'A' }),
+			React.createElement(Welcome, { name: 'B' }),
+			React.createElement(Welcome, { name: 'C' })
+		);
+	}
+
+	// render whole thing
+	ReactDOM.render(React.createElement(App, null), document.getElementById('not-root'));
+}
+if (document.querySelector('.section-two')) {
+	// do stuff
+}
--- react-docs/src/main.js
+++ react-docs/src/main.js
@@ -1,47 +1,88 @@
-(function() {
-
 // NB: any subsequent code snippets overwrite earlier ones
 
-/* ------------- 1. INTRO TO JSX ------------- */
+if (document.querySelector('.section-one')) {
+	/* ------------- 1. INTRO TO JSX ------------- */
 
-// function
-const formatName = user => { 
-	return user.firstName + ' ' + user.lastName
-};
+	// function
+	const formatName = user => { 
+		return user.firstName + ' ' + user.lastName
+	};
 
-// input object
-const user = {
-	firstName: 'Seb',
-	lastName: 'Gorka'
-};
+	// input object
+	const user = {
+		firstName: 'Seb',
+		lastName: 'Gorka'
+	};
 
-// JSX w/ embedded expression
-const greeting = (
-	<h1 className='react__demo'>
-	Hello, {formatName(user)}!
-	</h1>
-);
+	// JSX w/ embedded expression
+	const greeting = (
+		<h1 className='react__demo'>
+		Hello, {formatName(user)}!
+		</h1>
+	);
 
-ReactDOM.render(
-	greeting,
-	document.getElementById('root')
-);
+	ReactDOM.render(
+		greeting,
+		document.getElementById('root')
+	);
 
-/* ------------- 2. USING W/ FUNCTIONS ------------- */
+	/* ------------- 2. USING W/ FUNCTIONS ------------- */
 
-// conditionals
-function getGreeting(user) {
-	if (user.lastName == 'Gorka') {
-		return <h1>You are a Nazi, {user.firstName} {user.lastName}.</h1>;
+	// conditionals
+	function getGreeting(user) {
+		if (user.lastName == 'Gorka') {
+			return <h1>You are a Nazi, {user.firstName} {user.lastName}.</h1>;
+		}
+		else {
+			return <h1>Heeeyyyyyy {formatName(user)}</h1>;
+		}
 	}
-	else {
-		return <h1>Heeeyyyyyy {formatName(user)}</h1>;
+
+	ReactDOM.render(
+		getGreeting(user),
+		document.getElementById('root')
+	);
+
+	/* ------------- 3. UPDATING RENDERED ELEMS ------------- */
+
+	// create div displaying time
+	function tick() {
+		const elem = (
+			<div>
+				<h1>Hi!!!</h1>
+				<h2>The time rn is {new Date().toLocaleTimeString()}. :)</h2>
+			</div>
+		);
+		ReactDOM.render(elem, document.getElementById('root'));
 	}
+	// update 1x/second
+	setInterval(tick, 1000);
+
+	/* TAKEAWAY:
+	 * only the date displayed changes in browser DOM,
+	 * even tho we're "creating" a whole new UI */
+
+	/* ------------- 4. INTRO TO COMPONENTS ------------- */
+
+	// functional component
+	function Welcome(props) {
+		return <h1>Hi {props.name}!</h1>;
+	}
+
+	// compose components
+	function App() {
+		return (
+			<div>
+				<Welcome name="A" />
+				<Welcome name="B" />
+				<Welcome name="C" />
+			</div>
+		);
+	}
+
+	// render whole thing
+	ReactDOM.render(
+		<App />,
+		document.getElementById('not-root')
+	);
 }
-
-ReactDOM.render(
-	getGreeting(user),
-	document.getElementById('root')
-);
-
-})();
